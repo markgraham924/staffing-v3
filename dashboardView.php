18c649cb67a4dcce707be4540de097e8bd37fdab
Mark Graham <mgraham13@agsb.co.uk>
2019-09-30 10:14:40 +0100

    Update dashboardView.php

--- dashboardView.php
+++ dashboardView.php
@@ -27,14 +27,35 @@
                         $shiftTimes = $row["content"];
                         $startTime = date("g:ia j F", substr($shiftTimes, 0 , 10));
                         $endTime = date("g:ia j F", substr($shiftTimes, 12 , 10));
-                        echo '<br /><div class="container"><div class="alert alert-primary" role="alert"><h4 class="alert-heading">You are working soon!</h4><p>You next shift starts soon: '.$startTime.' - '.$endTime.'</p><hr><p class="mb-0">Hope it goes well :P</p></div></div>';
+                        echo '<br /><div class="container"><div class="alert alert-primary" role="alert"><h4 class="alert-heading">You are working soon!</h4><p>Your next shift starts soon: '.$startTime.' - '.$endTime.'</p><hr><p class="mb-0">Hope it goes well :P</p></div></div>';
                         $mainNotif = "done";
+                    } else {
+                        if ($mainNotif != "done") {
+                            $noNotif = TRUE;
+                        }
                     }
+                } else {
+                    if ($mainNotif != "done") {
+                        $noNotif = TRUE;
+                    }
+                }
+            } else {
+                if ($mainNotif != "done") {
+                    $noNotif = TRUE;
                 }
             }
         }
+    } else {
+        if ($mainNotif != "done") {
+            $noNotif = TRUE;
+        }
     }
     $conn->close();
+
+    if ($noNotif == TRUE) {
+        echo '<br /><div class="container"><div class="alert alert-primary" role="alert"><h4 class="alert-heading">You dont have any shifts scheduled!</h4><p>Check back later</p><hr><p class="mb-0">Have a good day :P</p></div></div>';
+        $mainNotif = "done";
+    }
 
 ?>
 
@@ -53,6 +74,7 @@
     </ul>
     <div id="dashboardTab" class="tab-content">
         <div class="tab-pane fade active show" id="notifications">
+            <br />
             <?php
 
                 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -62,23 +84,28 @@
 
                 $sql = "SELECT staffID, timeEnd, notifType, content FROM notifications ORDER BY timeEnd ASC";
 
+                $noOutput = TRUE;
                 $result = $conn->query($sql);
                 if ($result->num_rows > 0) {
                     while($row = $result->fetch_assoc()) {
                         if ($row["staffID"] == $staffID) {
                             if ($row["notifType"] ==  "nextShift") {
                                 $timeEndp1 = $row["timeEnd"];
-                                if ( $timeEndp1-time() >= 360) {
+                                if ( $timeEndp1-time() >=  0) {
                                     $shiftTimes = $row["content"];
                                     $startTime = date("g:ia j F", substr($shiftTimes, 0 , 10));
                                     $endTime = date("g:ia j F", substr($shiftTimes, 12 , 10));
-                                    echo '';
+                                    echo '<div class="card border-info mb-6" style="max-width: 20rem;"><div class="card-header">Shift</div><div class="card-body"><h4 class="card-title">You are working</h4><p class="card-text">Your next shift is: '.$startTime.' - '.$endTime.'</p></div></div>';
+                                    $noOutput = FALSE;
                                 }
                             }
                         }
                     }
                 }
                 $conn->close();
+                if ($noOutput == TRUE) {
+                    echo "<br /><h6>There are no notifications that require your attention</h6>";
+                }
 
             ?>
         </div>
